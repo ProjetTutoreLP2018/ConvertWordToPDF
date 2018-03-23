dce3c5c86ca0f1e157a6438755fa6887245c46de
ProjetTutoreLP2018 <37624544+ProjetTutoreLP2018@users.noreply.github.com>
2018-03-23 11:34:11 +0100

    Add files via upload

---
+++ WordToPDFWithoutInterop/WordToPDFWithoutInterop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordToPDF;
+namespace WordToPDFWithoutInterop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Word2Pdf ObjetWord = new Word2Pdf();
+            string dossier = "Z:\\projet tutore\\";
+            string nomDuFichierAConvertir = "cv.doc"; //ou docx
+            object CheminDuFichier = dossier + "\\" + nomDuFichierAConvertir;
+            string ExtensionDuFichier = Path.GetExtension(nomDuFichierAConvertir);
+            string ExtensionCible = nomDuFichierAConvertir.Replace(ExtensionDuFichier, ".pdf");
+            if (ExtensionDuFichier == ".doc" || ExtensionDuFichier == ".docx")
+            {
+                object DossierCible = dossier + "\\" + ExtensionCible;
+                ObjetWord.InputLocation = CheminDuFichier;
+                ObjetWord.OutputLocation = DossierCible;
+                ObjetWord.Word2PdfCOnversion();
+            }
+        }
+    }
+}
